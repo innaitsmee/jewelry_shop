c219758adb57b8b17123f8db001d27952ec22400
Inna Ponomarenko <pnmrk1995@gmail.com>
2024-07-07 22:20:38 +0300

    commit

--- code/header.js
+++ code/header.js
@@ -5,21 +5,32 @@ const [...dropdowns] = document.querySelectorAll(".dropdown");
 
 if (isMobile.any()) {
   dropdowns.forEach((item) => {
-    //create arrows and add class 'touch' to header__item
+    //add class 'touch' to header__item and create arrows
     if (!item.classList.contains("location-dropdown")) {
       item.parentElement.classList.add("touch");
-      item.insertAdjacentHTML("beforebegin", "<span class=arrow></span>");
-    }
+      item.insertAdjacentHTML(
+        "beforebegin",
+        "<button class='button touch__btn touch-btn' type=button><span class='arrow'></span></button>"
+      );
+      //add class 'touch-btn' to location svg
+    } else item.previousElementSibling.classList.add("touch-btn");
   });
 
-  //click on arrow and open dropdown
-  [...document.querySelectorAll(".arrow")].forEach((item) => {
-    item.addEventListener("click", function () {
+  //open dropdown
+  [...document.querySelectorAll(".touch-btn")].forEach((btn) => {
+    btn.addEventListener("click", function () {
+      //open dropdown and rotate arrow at header items
       toggleFunc(this.nextElementSibling, "active-dropdown");
-      //close all dropdowns were opened defore
+      if (this.parentElement.classList.contains("touch")) {
+        toggleFunc(this, "rotate");
+      }
+
+      //close all dropdowns were opened before
+      //rotate all arrows to start position (if item has arrow)
       dropdowns.forEach((item) => {
         if (item !== this.nextElementSibling) {
           item.classList.remove("active-dropdown");
+          item.previousElementSibling.classList.remove("rotate");
         }
       });
     });
